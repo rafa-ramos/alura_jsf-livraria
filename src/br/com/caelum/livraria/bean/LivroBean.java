6374a4f6cb826917d536ba75d5e7b30f4f3e4240
Rafael Ramos <rafaelramos@sparkag.com.br>
2020-01-09 09:50:54 -0300

    Projeto JSF, até aula 05

---
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -0,0 +1,79 @@
+package br.com.caelum.livraria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Autor;
+import br.com.caelum.livraria.modelo.Livro;
+
+@ManagedBean
+// ViewScoped quer dizer que não será criada uma nova instância para cada requisição. O default é RequestScoped que sempre incializa um novo obj para cada requisição.
+@ViewScoped
+public class LivroBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private Livro livro = new Livro();
+	private Integer autorId;
+	
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+	
+	public Integer getAutorId() {
+		return autorId;
+	}
+	
+	public Livro getLivro() {
+		return livro;
+	}
+	
+	public List<Livro> getLivros() {
+		return new DAO<Livro>(Livro.class).listaTodos();
+	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+		
+		System.out.println("Livro escrito por " + autor.getNome());
+		
+	}
+	
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+
+		if (livro.getAutores().isEmpty()) {
+			//throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor"));
+			return;
+		}
+
+		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		
+		   // aqui criamos um novo livro vazio pra limpar os campos e o id
+        this.livro = new Livro();
+	}
+	
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
+		String valor = value.toString();
+		if (!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+		}
+	}
+}
